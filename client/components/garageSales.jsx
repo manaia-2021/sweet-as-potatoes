5211b4ef8d0d4bfbc53b3857ae4e333e475dce0c
cody-newman-21 <code.rad.solutions@gmail.com>
2021-09-10 09:55:54 +1200

    Add divs and classnames to listings

--- client/components/garageSales.jsx
+++ client/components/garageSales.jsx
@@ -4,11 +4,21 @@ import { connect } from 'react-redux'
 function garageSales (props) {
 
   return (
+    <div className='list'>
     <ul>
       {props.sales.map(sale => (
-        <li key={sale.id}>{sale.title} {sale.street} {sale.suburb} {sale.city} {sale.description} {sale.date} {sale.start_time} {sale.end_time}</li>
+        <li key={sale.id}>
+          <div className='listing'>
+          {sale.title} 
+          <div className='address'>{sale.street} {sale.suburb} {sale.city}</div> 
+          <div className='description'>{sale.description}</div> 
+          <div className='date'>{sale.date}</div> 
+          <div className='time'>{sale.start_time} {sale.end_time}</div>
+          </div> 
+        </li>
       ))}
     </ul>
+    </div>
   )
 }
 
